583cb9b4149b6429cf5b2efebc9ca39d9e945e6b
TsaiYK <33308568+TsaiYK@users.noreply.github.com>
2025-01-12 12:14:54 -0600

    Add files via upload

---
+++ main_p3ga.m
@@ -0,0 +1,125 @@
+clear
+clc
+close all
+
+%% Add paths
+addpath('../src/')
+addpath('../src/utils/')
+W_constr_handling = true;
+if W_constr_handling
+    addpath(genpath('C:\Users\yktsai0121\Documents\GitHub\p3ga_constr_handling\p3ga'))
+    rmpath(genpath('C:\Users\yktsai0121\Documents\GitHub\p3ga\p3ga'))
+    addpath(genpath('C:\Users\yktsai0121\Documents\GitHub\p3ga_constr_handling\dd_tools'))
+    rmpath(genpath('C:\Users\yktsai0121\Documents\GitHub\p3ga\dd_tools'))
+else
+    addpath(genpath('C:\Users\yktsai0121\Documents\GitHub\p3ga\p3ga'))
+    rmpath(genpath('C:\Users\yktsai0121\Documents\GitHub\p3ga_constr_handling\p3ga'))
+    addpath(genpath('C:\Users\yktsai0121\Documents\GitHub\p3ga\dd_tools'))
+    rmpath(genpath('C:\Users\yktsai0121\Documents\GitHub\p3ga_constr_handling\dd_tools'))
+end
+addpath(genpath('C:\Users\yktsai0121\Documents\GitHub\randlc'))
+addpath(genpath('C:\Users\yktsai0121\Documents\GitHub\randlc\resources\cprnd')) % for some reason this doesn't work sometimes
+addpath(genpath('C:\Users\yktsai0121\Documents\GitHub\EPO'));
+addpath(genpath('C:\Users\yktsai0121\OneDrive - Texas A&M University\2022Fall\IEEE_Evo_Comp_2023\matlab_codes'))
+
+set(0,'DefaultTextInterpreter','latex'); % change the text interpreter
+set(0,'DefaultLegendInterpreter','latex'); % change the legend interpreter
+set(0,'DefaultAxesTickLabelInterpreter','latex'); % change the tick interpreter
+set(0, 'DefaultLineLineWidth', 1.5);
+set(groot,'defaultAxesXGrid','on')
+set(groot,'defaultAxesYGrid','on')
+set(0,'defaultAxesFontSize',14)
+
+%% Select a Design Strategy
+DesignStrategy = 'seqdesign';
+SystemType = '2D_numerical';
+
+mkdir('results')
+
+if strcmp(DesignStrategy,'CCD')
+    SaveLoc_dir = 'results\ccd_results';
+    xp = [];
+elseif strcmp(DesignStrategy,'plantdesign')
+    SaveLoc_dir = 'results\plantdesign_results';
+    xp = [];
+elseif strcmp(DesignStrategy,'controldesign')
+    SaveLoc_dir = 'results\controldesign_results';
+    switch SystemType
+        case '2D_numerical'
+            xp = 1; % if control design only, the plant design variable is fixed
+        case '4D_satellite'
+            xp = cosd(39.14); % if control design only, the plant design variable is fixed
+    end
+    
+elseif strcmp(DesignStrategy,'seqdesign')
+    % if plant design is done and we are going to do control design based on
+    % all the designed plants
+    SaveLoc_dir = 'results\seqdesign_results';
+    PlantOptDesign = load('results\plantdesign_results\xp_opt.mat'); % from the plant design optimization results
+    xp_opt = PlantOptDesign.xp_opt;
+    if strcmp(SystemType,'2D_numerical')
+%         theta_p_opt = 2.25*xp_opt(:,1).*sin(xp_opt(:,2)*pi)+2.75;
+%         xp_opt = PlantOptDesign.xp_opt;
+        rand_index = randi([1,size(xp_opt,1)],1,50);
+        theta_p_opt = 2.25*xp_opt(rand_index,1).*sin(xp_opt(rand_index,2)*pi)+2.75;
+    else
+        theta_p_opt = xp_opt;
+    end
+end
+
+if strcmp(DesignStrategy,'seqdesign')
+    x_opt = []; fval = [];
+    for i = 1:length(theta_p_opt)
+        [K_opt{i},fval_iter{i},M{i}] = runobjconstr_p3ga(theta_p_opt(i),SaveLoc_dir,DesignStrategy,i,W_constr_handling);
+        x_opt_tmp = [repmat(theta_p_opt(i),size(K_opt{i},1),1),K_opt{i}];
+        x_opt = [x_opt;x_opt_tmp];
+        fval = [fval;fval_iter{i}]; 
+    end
+else
+    [x_opt,fval,M] = runobjconstr_p3ga(xp,SaveLoc_dir,DesignStrategy,[],W_constr_handling);
+end
+
+%% Approximate PPD
+f1 = fval(:,1);
+f2 = fval(:,2);
+
+B = [f1,f2];
+x_fea = x_opt;
+x_fea(isnan(B(:,1)),:) = [];
+B(isnan(B(:,1)),:) = [];
+[A, varargout] = prtp(B);
+f1_ndom = A(:,1);
+f2_ndom = A(:,2);
+x_ndom = x_fea(varargout,:);
+
+%% Plot PF
+figure(1)
+hold on
+% p2 = plot(f1_PPD,f2_PPD,'ko');
+xlabel('$V_{N_s}$','Interpreter','Latex');
+ylabel('$||$eig($\Sigma_{\mathbf{e}})||$','Interpreter','Latex');
+
+[f1_PPD_sort, index_sort] = sort(f1_ndom);
+f2_PPD_sort = f2_ndom(index_sort);
+x_PPD_sort = x_ndom(index_sort,:);
+
+if strcmp(DesignStrategy,'CCD')
+    plot(f1_PPD_sort,f2_PPD_sort,'b-*'); % CCD
+    title('Design Strategy: CCD')
+elseif strcmp(DesignStrategy,'plantdesign')
+    plot(f1_PPD_sort,f2_PPD_sort,'-^','Color',[0.3010 0.7450 0.9330]); % only plant design
+    title('Design Strategy: Plant Design')
+elseif strcmp(DesignStrategy,'controldesign')
+    plot(f1_PPD_sort,f2_PPD_sort,'k-o'); % control design by fixing plant
+    title('Design Strategy: Control Design')
+elseif strcmp(DesignStrategy,'seqdesign')
+    plot(f1_PPD_sort,f2_PPD_sort,'-^','Color',[0.4660 0.6740 0.1880]); % seq design
+    title('Design Strategy: Sequential Design')
+end
+
+%% Save data
+save(strcat(SaveLoc_dir,'\p3ga_results_final.mat'))
+if strcmp(DesignStrategy,'plantdesign')
+    xp_opt = x_opt;
+    save(strcat(SaveLoc_dir,'\xp_opt.mat'),'xp_opt')
+end
---
+++ runobjconstr_p3ga.m
@@ -0,0 +1,175 @@
+function [x_opt,fval,M] = runobjconstr_p3ga(xp,SaveLoc_dir,DesignStrategy,i,W_constr_handling)
+
+xLast = []; % Last place computeall was called
+myf1 = []; % Use for objective at xLast
+myf2 = []; % Use for objective at xLast
+myf3 = []; % Use for constraint at xLast
+
+A = []; b = []; Aeq = []; beq = [];
+TolCon = 1e-6; classif_err_allowance = 0.1;
+
+%% P3GA Setting
+fun_all = @(x) [myfun1(x) myfun2(x)];    % objective function
+par = [];                          % the parameter function is in the first column of fun1and2
+dom = [1,2];                          % the dominator function is in the second column of fun1and2
+if strcmp(DesignStrategy,'CCD')
+    K_lb = [-1.2583,-2.2467];
+    K_ub = [-0.0948,-0.2351];
+    lb = [-1,-1,K_lb];
+    ub = [1,1,K_ub];
+elseif strcmp(DesignStrategy,'plantdesign')
+    K_lqr = [-0.6167,-1.2703];
+    % For Plant Design:
+    lb = [-1,-1];
+    ub = [1,1];
+elseif strcmp(DesignStrategy,'controldesign') || strcmp(DesignStrategy,'seqdesign')
+    K_lb = [-1.2583,-2.2467];
+    K_ub = [-0.0948,-0.2351];
+    lb = [K_lb];
+    ub = [K_ub];
+else
+    error('Unrecognizable design strategy!')
+end
+
+nvars = length(lb);                  % the number of design variables
+Generations = 3;                    % maximum number of generations
+PopulationSize = 5;                % maximum populations
+nonlcon = @(x) myfun3(x);
+% The constraint is set empty here because we assign NaN to both objectives
+% if the design is infeasible (including no infeasible solution for nominal
+% MPC, no disturbance set Z exists, and AK(=A+BK) is not exponentially stable)
+
+if ~W_constr_handling
+    options = p3gaoptimset('Generations',Generations,'PopulationSize',PopulationSize,...
+        'ViewProgress',true);
+    options.SaveLoc = SaveLoc_dir;
+else
+    % Initialize population
+    [X_new, feasible, initial, initialfeasible, cEval] = Initialize_LHS_GP(lb,ub,...
+            A,b,Aeq,beq,nonlcon,PopulationSize,nvars,'DontPlot',classif_err_allowance,TolCon,[]);
+    % Define options
+    options = p3gaoptimset('Generations',Generations,'PopulationSize',PopulationSize,...
+        'ViewProgress',true,'InitialPopulation',X_new,...
+        'CrossoverFraction',0.8,'MutationFraction',0.05);
+    options.initial = initial;
+    options.initialfeasible = initialfeasible;
+    options.GenerationPlots = 1;
+    options.classif_err_allowance = classif_err_allowance;
+    options.cEval = cEval;
+    options.TolCon = TolCon;
+    options.SaveLoc = SaveLoc_dir;
+end
+options.Log = true;
+options.GenerationData = true;
+options.GenerationDataIncrement = 2;
+options.Dominance = 'hch'; % determine if you want to use HCH-based dominance
+options.hvi_options.phi = 90; % Hypercone angle
+options.ref_bnds = [200,600;0,0.5];
+
+%% Run P3GA
+[x_opt,fval,M] = p3ga(fun_all,dom,par,nvars,[],[],[],[],lb,ub,nonlcon,options);
+figure(gcf); 
+saveas(figure(gcf),strcat(options.SaveLoc,'\p3ga_phvi.fig'))
+saveas(figure(gcf),strcat(options.SaveLoc,'\p3ga_phvi.png'))
+close(gcf)
+
+figure(gcf); 
+xlabel('$V_{N_s}$','Interpreter','Latex');
+ylabel('$||$eig($\Sigma_{\mathbf{e}})||_2$','Interpreter','Latex');
+zlabel('$u_{max}$','Interpreter','Latex')
+saveas(figure(gcf),strcat(options.SaveLoc,'\p3ga_results.fig'))
+saveas(figure(gcf),strcat(options.SaveLoc,'\p3ga_results.png'))
+close(gcf)
+
+if strcmp(DesignStrategy,'seqdesign')
+    save(strcat(options.SaveLoc,'\p3ga_results',num2str(i),'.mat'))
+else
+    save(strcat(options.SaveLoc,'\p3ga_results.mat'))
+end
+
+%% Private functions for objective
+    function [f1] = myfun1(x)
+        if ~isequal(x,xLast) % Check if computation is necessary
+            if strcmp(DesignStrategy,'plantdesign')
+                % For plant design
+                xp = 2.25*x(1)*sin(x(2)*pi)+2.75;
+%                 xp = x;
+                K = K_lqr;
+            elseif strcmp(DesignStrategy,'controldesign') || strcmp(DesignStrategy,'seqdesign')
+                % For Control Design
+                K = x(1:2);
+            else
+                % For CCD
+                xp = 2.25*x(1)*sin(x(2)*pi)+2.75;
+%                 xp = x(1);
+                K = x([3,4]);
+            end
+            [J,Size_Sigma_e] = func_tubeSMPC(xp,K,'2D_numerical');
+            myf1 = J;
+            myf2 = Size_Sigma_e;
+            myf3 = isnan(J);
+            xLast = x;
+        end
+        % Now compute objective function
+        f1 = myf1;
+    end
+
+
+    function [f2] = myfun2(x)
+        if ~isequal(x,xLast) % Check if computation is necessary
+            if strcmp(DesignStrategy,'plantdesign')
+                % For plant design
+                xp = 2.25*x(1)*sin(x(2)*pi)+2.75;
+%                 xp = x;
+                K = K_lqr;
+            elseif strcmp(DesignStrategy,'controldesign') || strcmp(DesignStrategy,'seqdesign')
+                % For Control Design
+                K = x(1:2);
+            else
+                % For CCD
+                xp = 2.25*x(1)*sin(x(2)*pi)+2.75;
+%                 xp = x(1);
+                K = x([3,4]);
+            end
+            [J,Size_Sigma_e] = func_tubeSMPC(xp,K,'2D_numerical');
+            myf1 = J;
+            myf2 = Size_Sigma_e;
+            myf3 = isnan(J);
+            xLast = x;
+        end
+        % Now compute objective function
+        f2 = myf2;
+    end
+
+    function [c,ceq] = myfun3(x)
+        if ~isequal(x,xLast) % Check if computation is necessary
+            if strcmp(DesignStrategy,'plantdesign')
+                % For plant design
+                xp = 2.25*x(1)*sin(x(2)*pi)+2.75;
+%                 xp = x;
+                K = K_lqr;
+            elseif strcmp(DesignStrategy,'controldesign') || strcmp(DesignStrategy,'seqdesign')
+                % For Control Design
+                K = x(1:2);
+            else
+                % For CCD
+                xp = 2.25*x(1)*sin(x(2)*pi)+2.75;
+%                 xp = x(1);
+                K = x([3,4]);
+            end
+            [J,Size_Sigma_e] = func_tubeSMPC(xp,K,'2D_numerical');
+            myf1 = J;
+            myf2 = Size_Sigma_e;
+            myf3 = isnan(J);
+            xLast = x;
+        end
+        % Now compute constraint function
+        if myf3 % infeasible design
+            c = 1;
+        else
+            c = -1;
+        end
+        ceq = [];
+    end
+    
+end
